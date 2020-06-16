5ed434f2c0cf32598081e113770e1f40d3f97498
mohammed ayodeji <ayslim007@gmail.com>
2020-06-16 14:38:40 +0100

    new commit

--- index.js
+++ index.js
@@ -3,11 +3,19 @@ const express = require("express");
 const server = express();
 const port = 3000;
 
+server.use(express.static("public/"));
+
+server.use(express.json());
+
 server.get("/", (req, res) => {
   res.send({
     name: "mohammed",
     age: 34,
   });
+});
+
+server.get("/name", (req, res) => {
+  console.log(req.body);
 });
 
 server.listen(port, () => {
